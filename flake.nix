a53ab27032aaf87975cd4ef10c371077f4212313
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-01-05 14:40:33 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,63 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.11";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = with import nixpkgs {
-        system = "x86_64-linux";
+      startPluto = pkgs.writeText "start_pluto.jl" ''
+        using Pkg;
+        if isfile("Project.toml") && isfile("Manifest.toml")
+            Pkg.activate(".")
+        end;
+        Pkg.instantiate()
+        using Pluto;
+        Pluto.run(notebook="/tmp/workdir/pid.jl")
+      '';
+    in
+    {
+
+
+      packages.${system}.pid-docker = pkgs.dockerTools.buildImage {
+        name = "guilloteauq/pid-jl-docker";
+        tag = "jan23";
+
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = [ pkgs.julia-stable-bin ];
+          pathsToLink = [ "/bin" ];
+        };
+        runAsRoot = ''
+            #!${pkgs.runtimeShell}
+          mkdir -p /tmp/workdir
+          cd /tmp/workdir
+          ln -s ${./Project.toml} /tmp/workdir/Project.toml
+          ln -s ${./Manifest.toml} /tmp/workdir/Manifest.toml
+          ln -s ${./pid.jl} /tmp/workdir/pid.jl
+      
+        '';
+        config = {
+          WorkingDir = "/tmp/workdir";
+          Cmd = [
+            "/bin/julia"
+            startPluto
+          ];
+          ExposedPorts = {
+            "1234/tcp" = { };
+          };
+        };
       };
-        [ julia-stable-bin ];
+
+
+      devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = with import nixpkgs
+          {
+            system = "x86_64-linux";
+          };
+          [ julia-stable-bin ];
         shellHook = ''
           julia -e '
             using Pkg;
@@ -19,8 +66,8 @@
             end;
             using Pluto;
             Pluto.run(notebook=joinpath(pwd(), "pid.jl"))'
-          '';
-    };
+        '';
+      };
 
-  };
+    };
 }
